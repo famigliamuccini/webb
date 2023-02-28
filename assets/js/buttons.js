6129f206a1873f6da44675033fe90914ebdc13ce
famigliamuccini <126505413+famigliamuccini@users.noreply.github.com>
2023-02-28 13:35:11 -0300

    1.0.3

--- assets/js/buttons.js
+++ assets/js/buttons.js
@@ -23,23 +23,31 @@ function linkedin() {
     window.open('https://www.linkedin.com/company/famigliamuccini/');
 }
 
+function facebook() {
+    console.log('Usuário direcionado ao Linkedin.');
 
+    window.open('https://www.facebook.com/FamigliaMuccini');
+}
 
+function youtube() {
+    console.log('Usuário direcionado ao Linkedin.');
 
-
-
+    window.open('https://www.youtube.com/@famigliamuccini5273');
+}
 
 const TXT = document.getElementById('meet')
 
-
 function next() {
-    TXT.innerHTML= (`
+
+    window.open('https://www.instagram.com/famigliamuccini/');
+
+    // TXT.innerHTML= (`
     
-    <h2>Teste</h2>
+    // <h2>Teste</h2>
          
-    <div id="buttonsLine">
-            <input type="button" value="Saber Mais" class="buttonB" id="nextButton">
-            <input type="button" value="Avançar" class="button" id="nextButton" onclick="next()">
-          </div>
-    `);
+    // <div id="buttonsLine">
+    //         <input type="button" value="Saber Mais" class="buttonB" id="nextButton">
+    //         <input type="button" value="Avançar" class="button" id="nextButton" onclick="next()">
+    //       </div>
+    // `);
 }
